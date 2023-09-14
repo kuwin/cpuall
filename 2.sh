257e0caf076dbb7ca82f9340bc6e96116d67d0b3
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-14 22:08:10 +0700

    2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 
 
-cd && apt update -y && apt upgrade -y && apt install nano && apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone https://github.com/kuwin/cpuminerall.git && cd cpuminerall && chmod +x run && chmod +x build.sh && chmod +x autogen.sh && chmod +x build-android.sh && ./build-android.sh && cd && cd cpuminerall && ./run 
+cd && apt update -y && apt upgrade -y && apt install nano && apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone https://github.com/kuwin/cpuminerall.git && cd cpuminerall && chmod +x run && chmod +x build.sh && chmod +x autogen.sh && chmod +x build-android.sh && ./build-android.sh && cd && cd cpuminerall && ./run 
 
