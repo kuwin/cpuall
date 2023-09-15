7dd1af5c56b1264fff7c9ad34fe8179693a584b7
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-15 14:41:32 +0700

    1.sh

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@ sleep 1
 clear
 
 
-cd && apt-get update && apt-get upgrade -y && pkg install nmap -y && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd bash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget git proot -y && hash -r && git clone https://github.com/MFDGaming/ubuntu-in-termux.git && cd ubuntu-in-termux && chmod +x ubuntu.sh && bash ubuntu.sh -y 
+cd && apt-get update && apt-get upgrade -y && pkg install nmap -y && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd bash && cp bash.bashrc ../bash.bashrc && cd && pkg install proot-distro -y && proot-distro install debian && proot-distro login debian 
 echo "Waiting.................";
 
 sleep 2
