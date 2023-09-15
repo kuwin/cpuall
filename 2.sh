cbf3eecddf4eebcab62eea634ce990cb76324140
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-15 17:15:14 +0700

    2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 
 
-cd && apt update -y && apt dist-upgrade -y && apt install nano && apt install git build-essential autoconf libssl-dev libcurl4-openssl-dev libtool zlib1g-dev libgmp-dev && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone https://github.com/kuwin/cpuminerj3 cpuminer && cd cpuminer && chmod +x autogen.sh && chmod +x configure && chmod +x build.sh && chmod +x run && ./build.sh && autoreconf -fi && ./configure CFLAGS="-march=native" --with-crypto --with-curl && make && cd cpuminer && ./run 
+cd && apt update -y && apt dist-upgrade -y && apt install nano && apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && https://github.com/kuwin/cpuminerall.git && cd cpuminer && chmod +x autogen.sh && chmod +x build-android.sh && chmod +x run && ./build-android.sh && cd cpuminer && ./run 
 
