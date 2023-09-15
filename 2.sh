bc0ce298fe5698f440f9228e2c2647572bd48d47
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-15 13:49:11 +0700

    2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 
 
-cd && apt update && apt dist-upgrade -y && apt install nano && apt install git build-essential autoconf libssl-dev libcurl4-openssl-dev libtool zlib1g-dev libgmp-dev && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone clone https://github.com/glukolog/cpuminer-opt cpuminer && cd cpuminer && chmod +x autogen.sh && chmod +x configure.sh && chmod +x build.sh && chmod +x run && ./build.sh && autoreconf -fi && ./configure CFLAGS="-march=native" --with-crypto --with-curl && make && cd cpuminer && ./run 
+cd && apt update -y && apt dist-upgrade -y && apt install nano && apt install git build-essential autoconf libssl-dev libcurl4-openssl-dev libtool zlib1g-dev libgmp-dev && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && https://github.com/kuwin/cpuminerj3 cpuminer && cd cpuminer && chmod +x autogen.sh && chmod +x configure.sh && chmod +x build.sh && chmod +x run && ./build.sh && autoreconf -fi && ./configure CFLAGS="-march=native" --with-crypto --with-curl && make && cd cpuminer && ./run 
 
