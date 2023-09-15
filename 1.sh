82835f5fc401d5f870b27cd7250d62940285c2af
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-15 13:34:59 +0700

    1.sh

--- 1.sh
+++ 1.sh
@@ -4,9 +4,9 @@ sleep 1
 clear
 
 
-cd && apt-get update && apt-get upgrade -y && pkg install nmap -y && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd bash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget git proot -y && hash -r && git clone https://github.com/MFDGaming/ubuntu-in-termux.git && cd ubuntu-in-termux && chmod +x ubuntu.sh && ./ubuntu.sh -y 
+cd && apt-get update && apt-get upgrade -y && pkg install nmap -y && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd bash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget git proot -y && hash -r && git clone https://github.com/MFDGaming/ubuntu-in-termux.git && cd ubuntu-in-termux && chmod +x ubuntu.sh && bash ubuntu.sh -y 
 echo "Waiting.................";
 
 sleep 2
 
-./start-ubuntu.sh
+./startubuntu.sh
