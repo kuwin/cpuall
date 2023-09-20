4020ac567c7433860af2eedb87cdc8cb6210282a
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-20 13:09:45 +0700

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,12 @@
+
+echo "Warning! Reg code with Y/yes only";
+sleep 1
+clear
+
+
+cd && pkg update -y && pkg upgrade -y && pkg install git -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install proot-distro -y && proot-distro install debian -y && proot-distro login debian && adduser hi && sudo su
+echo "Waiting.................";
+
+sleep 2
+
+
