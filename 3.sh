396c02d189552c90ec7ea6d066111cb15f526370
kuwin <54054246+kuwin@users.noreply.github.com>
2023-09-20 13:23:54 +0700

    3.sh

--- 3.sh
+++ 3.sh
@@ -4,7 +4,7 @@ sleep 1
 clear
 
 
-cd && pkg update -y && pkg upgrade -y && pkg install git -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install proot-distro -y && pkg proot-distro install debian -y && proot-distro login debian && adduser hi && sudo su
+cd && pkg update -y && pkg upgrade -y && pkg install git -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bash.git && rm -rf bash.bashrc && cd bash && cp bash.bashrc ../bash.bashrc && cd && pkg install proot-distro -y && pkg proot-distro install debian -y && proot-distro login debian && adduser hi && sudo su
 echo "Waiting.................";
 
 sleep 2
